ac9dce3c21eeb95b944447c8b5a1cffe00510357
Ohkubo KOHEI <o@kbn.one>
2026-05-03 15:43:14 +0000

    rm all files

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'preact';
-import './styles/global.css';
-import { App } from './App';
-
-const root = document.getElementById('app');
-if (!root) throw new Error('#app missing');
-render(<App />, root);
